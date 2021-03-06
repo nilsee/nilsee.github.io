4cbc71d0f9210671f2d83fb71de3fc5778118000
Nils Einar Eide <nils@dataingenioer.no>
2021-03-06 17:06:21 +0100

    Update components.js

--- js/components.js
+++ js/components.js
@@ -1,5 +1,15 @@
-class App extends React.Component {
+class HelloWorld extends React.Component {
   render() {
     return <h1>Hello world!</h1>;
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+        <div>
+          <HelloWorld />
+        </div>
+    );
   }
 }
