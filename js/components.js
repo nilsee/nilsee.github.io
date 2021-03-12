ad177d809403e54ce513320d079d6e935ad47aa5
Nils Einar Eide <nils@dataingenioer.no>
2021-03-12 14:44:50 +0100

    Update components.js
    Added clock

--- js/components.js
+++ js/components.js
@@ -4,11 +4,49 @@ class HelloWorld extends React.Component {
   }
 }
 
+class Clock extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date()
+        };
+    }
+
+    componentDidMount () {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount () {
+        clearInterval(this.timerID);
+    }
+
+    tick () {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render () {
+        return (
+            <div className="clock">
+                {this.state.date.toLocaleTimeString()}
+            </div>
+        );
+    }
+}
+
 class App extends React.Component {
   render() {
     return (
         <div id="helloworld">
           <HelloWorld />
+        </div>
+        <div>
+          <Clock />
         </div>
     );
   }
